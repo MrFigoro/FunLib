aa4caba7aa5634b6c6a0e8389f1417041bc7098e
Ihar Shabanovich <mrfigoro@gmail.com>
2019-03-26 19:32:28 +0300

    Close /user for unlogined users

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -9,6 +9,11 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class UserController extends Controller
 {
 	use SoftDeletes;
+
+	public function __construct()
+	{
+		$this->middleware('auth');
+	}
 	/**
 	 * Display a listing of the resource.
 	 *
--- routes/web.php
+++ routes/web.php
@@ -18,16 +18,12 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-
 Route::get('/loginform', 'Web\UserController@login')->name('loginform');
-
 Route::post('/signin', 'Web\UserController@signin')->name('signin');
 
-
-
-Route::get('user/', 'Admin\NewUserController@index')->name('index');
-Route::get('user/create', 'Admin\NewUserController@create')->name('create');
-Route::post('user/store', 'Admin\NewUserController@store')->name('save');
-Route::get('user/edit/{id?}', 'Admin\NewUserController@edit')->name('edit');
-Route::get('user/delete/{id}', 'Admin\NewUserController@destroy')->name('delete');
+Route::get('user', 'Admin\UserController@index')->name('index');
+Route::get('user/create', 'Admin\UserController@create')->name('create');
+Route::post('user/store', 'Admin\UserController@store')->name('save');
+Route::get('user/edit/{id?}', 'Admin\UserController@edit')->name('edit');
+Route::get('user/delete/{id}', 'Admin\UserController@destroy')->name('delete');
 
